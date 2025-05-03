42645bcf5cdb331becc74e2a2655ab3658ea46ec
valmir23 <valmirgashi931@gmail.com>
2025-05-03 05:45:03 -0700

    javascript

---
+++ day 22/main.js
@@ -0,0 +1,48 @@
+document.write("Hello World");
+ 
+var x = 10;
+var name = "Ariana";
+
+document.write(x);
+console.log(name);
+
+var a = 50 + 50;
+console.log(a);
+
+var y = 50;
+var z = 100;
+var yz = y + z;
+
+console.log(yz);
+
+var n = 99;
+n++;
+console.log(n);
+
+
+var o = 50;
+o++;
+console.log(o);
+
+var p = 20;
+var q = p ** 2;
+console.log(q);
+
+var m = 15;
+m+=5;
+console.log(m);
+
+
+var r = 35;
+r*=5;
+console.log(r);
+
+var num1 = 5;
+var num2 = 10;
+var num3 = 5;
+console.log(num1 != num2); //true
+console.log(num1 == num3); //true
+console.log(num1 > num3); //false
+console.log(num1 >= num3); //true
+console.log(num1 < num2); //true
+console.log(num1 <= num2); //true

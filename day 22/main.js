49ad462b68e898a79acfbf138b50ece61ef88ad6
valmir23 <valmirgashi931@gmail.com>
2025-05-10 04:30:23 -0700

    .

--- day 22/main.js
+++ day 22/main.js
@@ -37,6 +37,7 @@ var r = 35;
 r*=5;
 console.log(r);
 
+
 var num1 = 5;
 var num2 = 10;
 var num3 = 5;
@@ -46,3 +47,23 @@ console.log(num1 > num3); //false
 console.log(num1 >= num3); //true
 console.log(num1 < num2); //true
 console.log(num1 <= num2); //true
+
+
+var firstName= "Digital";
+var lastName = "School";
+
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var test = "Test123";
+console.log(`Hello from ${test}`);
+
+
+var age = 22;
+var age = 24;
+console.log(age >= 18 && age<=65);// and operator
+console.log(age >= 28 || age<=65);// or operator
+console.log(!(age == age2))
+
+
+
